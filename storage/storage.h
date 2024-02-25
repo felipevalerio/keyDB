50d223a650de72af1f96835b98851d1feba46a44
Felipe <felipevalerioderamos@gmail.com>
2024-02-25 18:07:36 -0300

    defining the functions in the header file

---
+++ storage/storage.h
@@ -0,0 +1,19 @@
+#include <map>;
+#include <string>;
+
+namespace storage {
+
+	class storage {
+
+		public:
+			void put(std::map<std::string, std::string>, std::string, std::string);
+
+		
+		public:
+			void del(std::string, std::map<std::string, std::string>);
+
+		
+		public:
+			void get_all(std::map<std::string, std::string>);
+	};
+}
