1dbe0c00bb98dffa51e7a2d232e9db81154b8443
Felipe <felipevalerioderamos@gmail.com>
2024-02-27 20:06:31 -0300

    solving some compiler errors when I try to compile the project

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include "storage/storage.h"
 
 
 int main() {
--- storage/storage.h
+++ storage/storage.h
@@ -1,19 +1,12 @@
 #include <map>
 #include <string>
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
 
-namespace storage {
+void put(std::map<std::string, std::string>, std::string, std::string);
 
-	class storage {
+void get_all(std::map<std::string, std::string>);
 
-		public:
-			void put(std::map<std::string, std::string>, std::string, std::string);
+void del(std::string, std::map<std::string, std::string>);
 
-		
-		public:
-			void del(std::string, std::map<std::string, std::string>);
-
-		
-		public:
-			void get_all(std::map<std::string, std::string>);
-	};
-}
+#endif
