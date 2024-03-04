55849c6f0f5a50e6b1014c2e64e2ff9e1ec706ae
Felipe <felipevalerioderamos@gmail.com>
2024-03-03 23:29:28 -0300

    still working

--- src/storage/storage.hpp
+++ src/storage/storage.hpp
@@ -1,12 +1,8 @@
 #include <map>
 #include <string>
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
 
-void put(std::map<std::string, std::string>, std::string, std::string);
+	void put(std::map<std::string, std::string>, std::string, std::string);
 
-void get_all(std::map<std::string, std::string>);
+	void get_all(std::map<std::string, std::string>);
 
-void del(std::string, std::map<std::string, std::string>);
-
-#endif
+	void del(std::string, std::map<std::string, std::string>);
